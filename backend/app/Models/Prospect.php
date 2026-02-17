749cb46264c70245ffbe739ac7c2fa6476792284
shaheda-a <seemabellary292@gmail.com>
2026-02-17 12:28:34 +0530

    Updated prospect list and added migration tables to work prospect

--- backend/app/Models/Prospect.php
+++ backend/app/Models/Prospect.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Prospect extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'company_name', 'industry', 'market_segment', 'customer_group',
