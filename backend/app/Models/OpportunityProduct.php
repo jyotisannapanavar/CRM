9b4d15914684551549f1f3de611b0136bae6cf45
jyotisannapanavar <136812071+jyotisannapanavar@users.noreply.github.com>
2026-02-19 11:43:43 +0530

    opportunity product added

---
+++ backend/app/Models/OpportunityProduct.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OpportunityProduct extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'opportunity_products';
+
+    protected $fillable = [
+        'opportunity_id',
+        'product_id',
+    ];
+
+    public function opportunity(): BelongsTo
+    {
+        return $this->belongsTo(Opportunity::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
